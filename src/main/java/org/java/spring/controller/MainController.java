96251056cfd3b542034715722b932513668cf3d2
marcobusacca <marcobusacca01@gmail.com>
2023-11-28 13:35:34 +0100

    Step 1 Completato

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -1,8 +1,19 @@
 package org.java.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+	@GetMapping("/")
+	public String page1(Model model) {
+		
+		String name = "Marco";
+		
+		model.addAttribute("name", name);
+		
+		return "page1";
+	}
 }
